f32dbff38e55ff492444b00b686a5a1ecb60da76
zacro14 <sacrojunel@protonmail.com>
2024-03-19 14:20:16 +0800

    feat: add the employee list

--- src/renderer/screens/Employees/index.tsx
+++ src/renderer/screens/Employees/index.tsx
@@ -1,3 +1,22 @@
+import { Plus } from "lucide-react";
+import { Button } from "renderer/components/ui/button";
+
 export const Employees = () => {
-  return <>employees</>;
+  return (
+    <div className="w-[calc(100vw-4rem)]">
+      <div className="border-b p-5 h-20 ">
+        <div className="flex justify-between">
+          <h2 className="scroll-m-20 pb-2 text-xl font-bold  first:mt-0">
+            Employee list
+          </h2>
+          <Button>
+            <Plus className="mr-2" />
+            New employee
+          </Button>
+        </div>
+      </div>
+
+      <div>employee</div>
+    </div>
+  );
 };
--- src/renderer/screens/Main/index.tsx
+++ src/renderer/screens/Main/index.tsx
@@ -1,6 +1,6 @@
 import { AreaChart, ArrowRight, CalendarCheck, UserRound } from "lucide-react";
 import { HtmlHTMLAttributes, ReactNode } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   Card,
   CardContent,
@@ -71,8 +71,16 @@ const DashboardCard = ({
 
 export function MainScreen() {
   return (
-    <section>
-      <div className="flex justify-center items-center h-dvh w-dvw gap-5 flex-wrap">
+    <section className="h-dvh w-[calc(100vw-4rem)] flex justify-center items-center  gap-5 flex-col">
+      <div className="py-2 md:py-10 flex flex-col gap-1">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+          Welcome to eLeave tracker app.
+        </h1>
+        <p className="text-xl text-muted-foreground text-center">
+          Track your employee with ease.
+        </p>
+      </div>
+      <div className="flex items-center justify-center flex-wrap gap-3">
         {dashboard.map((dashboardData) => (
           <DashboardCard {...dashboardData} />
         ))}
