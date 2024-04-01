55f5739abf6c546fbdcce6690b5a4ccc2a34985f
zacro14 <sacrojunel@protonmail.com>
2024-04-01 14:51:53 +0800

    feat: add employee form

--- src/renderer/components/organism/Dialog/AddEmployee.tsx
+++ src/renderer/components/organism/Dialog/AddEmployee.tsx
@@ -1,3 +1,5 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   DialogContent,
   DialogDescription,
@@ -5,22 +7,63 @@ import {
   DialogTitle,
 } from "renderer/components/ui/dialog";
 import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "renderer/components/ui/form";
+import { Input } from "renderer/components/ui/input";
+import { Button } from "renderer/components/ui/button";
 
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
 });
+
+type TAddEmployee = z.infer<typeof formSchema>;
+
 export default function AddEmployee() {
+  const form = useForm<TAddEmployee>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <DialogContent>
-      <DialogHeader>
-        <DialogTitle>Are you absolutely sure?</DialogTitle>
-        <DialogDescription>
-          This action cannot be undone. This will permanently delete your
-          account and remove your data from our servers.
-        </DialogDescription>
-      </DialogHeader>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </DialogContent>
   );
 }
